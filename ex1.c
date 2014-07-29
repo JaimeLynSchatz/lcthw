d0053848c4ebbf8b78ca819222ddf4c3d244fa8e
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2014-07-28 23:37:44 -0700

    first exercises in LCTHW

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  puts ("Hello world.");
+  puts ("C really is a wonderful language.");
+  puts ("I wonder what it would take to build an AI in C.");
+  puts ("What do you think, Bob?\n");
+  puts ("jaimelyn@jaimelyn-Aspire-5742:~/lcthw$ i think that is a great idea!");
+  return 0;
+}
